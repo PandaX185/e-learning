28deb8c9d5ba514b9fa98fbc785dc60dbe0a467c
PandaX185 <mouss3abbb@gmail.com>
2024-09-23 02:59:45 +0300

    chore: Update existing student with new teacher ID if not already associated

--- student/service/student_service.js
+++ student/service/student_service.js
@@ -5,10 +5,16 @@ export async function createStudent(student) {
     const hashedPassword = await bcrypt.hash(student.password, 0);
     student.hashedPassword = hashedPassword;
 
-    const existingStudent = await Student.findOne({ email: student.email, teacherId: student.teacherId });
+    const existingStudent = await Student.findOne({ email: student.email });
     if (existingStudent) {
-        throw new Error('Email already signed up');
-    }
+        if (!existingStudent.teachers.includes(student.teacherId)) {
+            existingStudent.teachers.push(student.teacherId);
+            await existingStudent.save();
+            return existingStudent;
+        } else {
+            throw new Error('Student already exists');
+        }
+    }       
 
     return Student.create(student);
 }
